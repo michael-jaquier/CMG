b87debbf604967b0b0f03b7a14e4c56e8bf0e99c
Michael Jaquier <michael.jaquier@nexthink.com>
2025-03-06 08:55:24 +0100

    chore: cleanup api

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap, fmt::Display};
+use std::fmt::Display;
 
 use rand::Rng;
 use serde::Serialize;
@@ -44,16 +44,14 @@ impl From<ProblemType> for Symb {
     }
 }
 
-pub fn generate_numbers(pl: ProblemLevel) -> Vec<u32> {
+fn generate_numbers(pl: ProblemLevel) -> Vec<u32> {
     let mut rng = rand::rng();
     let max_value = 10_u32.pow(pl as u32);
-    vec![
-        rng.random_range(1..=max_value),
-        rng.random_range(1..=max_value),
-    ]
+    let ns = (pl as usize).max(2);
+    (0..ns).map(|_| rng.random_range(1..=max_value)).collect()
 }
 
-pub fn problems(pl: ProblemLevel, pt: ProblemType) -> Question {
+fn problems(pl: ProblemLevel, pt: ProblemType) -> Question {
     let n = generate_numbers(pl);
     let v = n.into_iter().map(|zn| (zn, pt)).collect();
     let tokens = TokenRepr::from_problem(v);
@@ -70,7 +68,7 @@ pub struct Request {
 }
 
 #[allow(dead_code)]
-fn request(r: Option<Request>) -> Var {
+pub fn request(r: Option<Request>) -> Var {
     let (pl, pt) = match r {
         Some(r) => (r.plevel, r.ptype),
         None => (ProblemLevel::default(), ProblemType::default()),
@@ -84,14 +82,11 @@ fn request(r: Option<Request>) -> Var {
 }
 
 #[derive(Serialize, Clone, Default, Debug, Copy, PartialEq)]
-pub enum Symb {
+enum Symb {
     Plus,
     Minus,
     Multiplication,
     Division,
-    Equals,
-    LBracket,
-    RBracket,
     #[default]
     End,
 }
@@ -103,9 +98,6 @@ impl Display for Symb {
             Symb::Minus => "-",
             Symb::Multiplication => "*",
             Symb::Division => "/",
-            Symb::Equals => "=",
-            Symb::LBracket => "(",
-            Symb::RBracket => ")",
             Symb::End => "\n",
         };
         write!(f, "{}", s)
@@ -113,7 +105,7 @@ impl Display for Symb {
 }
 
 #[derive(Serialize, Clone, Default, Debug, PartialEq)]
-pub enum TokenRepr {
+enum TokenRepr {
     Symbol(Symb),
     Number(u32),
     #[default]
@@ -121,7 +113,7 @@ pub enum TokenRepr {
 }
 
 impl TokenRepr {
-    pub fn to_answer(question: &Question) -> Answer {
+    fn to_answer(question: &Question) -> Answer {
         let mut n: f32 = 0.0;
         let mut action = Symb::End;
         let tokens = question.tokens();
@@ -133,9 +125,6 @@ impl TokenRepr {
                     Symb::Minus => n -= *zn as f32,
                     Symb::Multiplication => n *= *zn as f32,
                     Symb::Division => n /= *zn as f32,
-                    Symb::Equals => {}
-                    Symb::LBracket => {}
-                    Symb::RBracket => {}
                     Symb::End => n += *zn as f32,
                 },
                 TokenRepr::End => {}
@@ -147,49 +136,18 @@ impl TokenRepr {
         }
     }
 
-    pub fn from_problem(n: Vec<(u32, ProblemType)>) -> Vec<TokenRepr> {
+    fn from_problem(n: Vec<(u32, ProblemType)>) -> Vec<TokenRepr> {
         let mut tokens = Vec::new();
         for (number, pt) in n.into_iter() {
             tokens.push(TokenRepr::Number(number));
             tokens.push(TokenRepr::Symbol(pt.into()))
         }
+        tokens.pop();
+
         tokens
     }
 
-    pub fn from_expression(expression: &str) -> CgmResult<Vec<TokenRepr>> {
-        let mut tokens = Vec::new();
-        let symbol_map: HashMap<&str, Symb> = [
-            ("+", Symb::Plus),
-            ("-", Symb::Minus),
-            ("*", Symb::Multiplication),
-            ("/", Symb::Division),
-            ("=", Symb::Equals),
-            ("(", Symb::LBracket),
-            (")", Symb::RBracket),
-        ]
-        .iter()
-        .cloned()
-        .collect();
-
-        fn parse_integer(token: &str) -> CgmResult<u32> {
-            token
-                .parse::<u32>()
-                .map_err(|_| CgmError::ParseIntegerError(token.to_string()))
-        }
-
-        for token in expression.split_whitespace() {
-            let parsed_token = if let Some(symb) = symbol_map.get(token) {
-                TokenRepr::Symbol(*symb)
-            } else {
-                TokenRepr::Number(parse_integer(token)?)
-            };
-            tokens.push(parsed_token);
-        }
-
-        Ok(tokens)
-    }
-
-    pub fn to_expression(tokens: &Vec<TokenRepr>) -> String {
+    fn to_expression(tokens: &Vec<TokenRepr>) -> String {
         let mut s = "".to_string();
         for token in tokens {
             match token {
@@ -252,25 +210,6 @@ mod tests {
     }
 
     #[test]
-    fn tokenize() {
-        use TokenRepr::*;
-        let tokens = "1 + 2 - 4";
-        let tokenized = TokenRepr::from_expression(tokens).unwrap();
-        assert_eq!(
-            tokenized,
-            vec![
-                Number(1),
-                Symbol(Symb::Plus),
-                Number(2),
-                Symbol(Symb::Minus),
-                Number(4)
-            ]
-        );
-        let untoken = TokenRepr::to_expression(&tokenized);
-        assert_eq!(untoken, "1+2-4")
-    }
-
-    #[test]
     fn make_request() {
         for _ in 0..100 {
             let req = Request {
@@ -278,7 +217,14 @@ mod tests {
                 ptype: ProblemType::Addition,
             };
             let r = request(Some(req));
-            assert!(r.answer.numerical <= 20.0);
+            assert!(r.answer.numerical <= 20.0, "{:?}", r);
         }
+    }
+
+    #[test]
+    fn problem_three() {
+        let pl = ProblemLevel::Three;
+        let r = problems(pl, ProblemType::Addition);
+        assert_eq!(r.tokens().len(), 5, "{:?}", r);
     }
 }
